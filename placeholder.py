30f9ba0644416d3f503c53ddff5db32f55017098
evilae <xd1nxm@gmail.com>
2024-10-11 14:43:11 +0300

    syntax error (line 45)

--- placeholder.py
+++ placeholder.py
@@ -42,7 +42,7 @@ elif choice == '2':
 elif choice == '3':
     result_3 = multiply(num1, num2)
     wtv_usr_put_3 = int(input(f"how much is {num1}  {num2}"))
-    if wtv_usr_put_3 == result_3
+    if wtv_usr_put_3 == result_3:
         print("chat is this real 󱜹 !")
     else:
         print("never touch this file again")
